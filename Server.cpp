011d6a36ec84ffe8929c882022fa01af7e0508e2
Bobby <s215818@dtu.dk>
2023-11-20 04:13:17 +0100

    Misc

--- Server.cpp
+++ Server.cpp
@@ -1,3 +1,4 @@
+#include "Server.h"
 #include <iostream>
 #include <vector>
 #include <string>
@@ -11,14 +12,6 @@ using namespace std;
 
 #define MAX_LEN 200
 #define NUM_COLORS 6
-
-void handle_error(const char* message);
-void set_name(int id, char name[]);
-void shared_print(string str);
-void broadcast_message(string message, int sender_id);
-void end_connection(int id);
-void handle_client(SOCKET client_socket_, int id);
-int run_server();
 
 struct terminal {
     int id;
