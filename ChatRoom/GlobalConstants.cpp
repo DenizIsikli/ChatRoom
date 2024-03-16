57e80c7a1e8cd481988eb5725414e27ceb7f83fc
Bobby <s215818@dtu.dk>
2024-03-16 15:16:02 +0100

    Global constants

---
+++ ChatRoom/GlobalConstants.cpp
@@ -0,0 +1,6 @@
+#include "GlobalConstants.h"
+
+
+const std::string SERVER_IP = "127.0.0.1";
+const int SERVER_PORT = 8080;
+bool exit_flag = false;
---
+++ ChatRoom/GlobalConstants.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+extern const std::string SERVER_IP;
+extern const int SERVER_PORT;
+extern bool exit_flag;
